ffa58eafb22b86ede72cc2fd170b0ae372bddd4b
Ren Ant <eurekavixen@gmail.com>
2024-09-19 10:52:46 +0800

    added student info prompt

--- main.rb
+++ main.rb
@@ -1,1 +1,17 @@
 require_relative 'student'
+
+puts "Enter id:"
+id = gets.to_i
+
+puts "Enter name:"
+name = gets.chomp
+
+puts "Enter birth date:"
+birth_date = gets.chomp
+
+puts "Enter email:"
+email = gets.chomp
+
+puts "Enter phone number:"
+phone_number = gets.chomp
+
