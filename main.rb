8a38e47950e098e8b17dab58431b8c9252a26a43
Ren Ant <eurekavixen@gmail.com>
2024-09-19 12:01:46 +0800

    added option to choose between student and course management

--- main.rb
+++ main.rb
@@ -4,41 +4,54 @@ require_relative 'course'
 option = nil
 while option != 'exit'
   puts "Choose an option below:\n"
-  puts "(1) Add Student\n(2) Delete Student\n"
+  puts "(1) Student Management\n(2) Course Management\n"
   option = gets.chomp
 
   case option.to_i
   when 1
-    id = Student.all.size + 1
+    puts "Choose an option below:\n"
+    puts "(1) Add Student\n(2) Delete Student\n"
+    option = gets.chomp
 
-    puts "Enter name:"
-    name = gets.chomp
+    case option.to_i
+    when 1
+      id = Student.all.size + 1
 
-    puts "Enter birth date:"
-    birth_date = gets.chomp
+      puts "Enter name:"
+      name = gets.chomp
 
-    puts "Enter email:"
-    email = gets.chomp
+      puts "Enter birth date:"
+      birth_date = gets.chomp
 
-    puts "Enter phone number:"
-    phone_number = gets.chomp
+      puts "Enter email:"
+      email = gets.chomp
 
-    student = Student.new
-    student.id = id
-    student.name = name
-    student.birth_date = birth_date
-    student.email = email
-    student.phone_number = phone_number
+      puts "Enter phone number:"
+      phone_number = gets.chomp
 
-    puts "Student added successfully!\n" if student.save
+      student = Student.new
+      student.id = id
+      student.name = name
+      student.birth_date = birth_date
+      student.email = email
+      student.phone_number = phone_number
 
+      if student.save
+        student.display
+        puts "Student added successfully!\n"
+      end
+
+    when 2
+      puts "Enter student id:"
+      id = gets.to_i
+
+      student = Student.find(id)
+      puts "Student destroyed successfully!\n" if student.destroy
+
+    end
   when 2
-    puts "Enter student id:"
-    id = gets.to_i
-
-    student = Student.find(id)
-    puts "Student destroyed successfully!\n" if student.destroy
 
   end
+
 end
 
