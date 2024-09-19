f09bb9963b4424126160398f8b7f2d2024e393e5
Ren Ant <eurekavixen@gmail.com>
2024-09-19 13:08:13 +0800

    added initialize method

---
+++ teacher.rb
@@ -0,0 +1,12 @@
+class Teacher
+  attr_accessor :id, :name, :birth_date, :email, :phone_number, :department
+
+  def initialize(id = nil, name = nil, birth_date = nil, email = nil, phone_number = nil, department = nil)
+    @id = id
+    @name = name
+    @birth_date = birth_date
+    @email = email
+    @phone_number = phone_number
+    @department = department
+  end
+end
