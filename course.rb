48ba50ae5a435333343fa3c88a607491efe9e05d
Ren Ant <eurekavixen@gmail.com>
2024-09-20 09:57:17 +0800

    modified course methods to filter soft deleted records

--- course.rb
+++ course.rb
@@ -1,37 +1,46 @@
 class Course
-  attr_accessor :id, :name
+  attr_accessor :id, :name, :deleted_at
 
-  def initialize(id = nil, name = nil)
+  def initialize(id = nil, name = nil, deleted_at = nil)
     @id = id
     @name = name
+    @deleted_at = deleted_at
   end
 
-  @@record = []
+  @@record = [
+    Course.new(1, 'c1'),
+    Course.new(2, 'c2'),
+  ]
 
   def save
     @@record.prepend(self)
   end
 
-  def self.first(query)
-    query ? @@record[0..query - 1].each { |course| course.display } : self.all.each { |course| course.display }
-  end
-
   def destroy
-    @@record.delete(self)
+    course = @@record.find { |course| course == self }
+    course.deleted_at = Time.now if course
   end
 
   def display
-    puts "===============\n"
     puts "ID: #{@id}"
     puts "Name: #{@name}"
     puts "===============\n"
   end
 
   def self.all
-    @@record
+    @@record.select { |course| !course.deleted_at }
+  end
+
+  def self.first(query)
+    query ? @@record[0..query - 1].each { |course| course.display unless course.deleted_at }
+      : self.all.each { |course| course.display unless course.deleted_at }
   end
 
   def self.find(id)
-    @@record.find { |student| student.id == id }
+    @@record.find { |course| course.id == id && !course.deleted_at }
+  end
+
+  def self.find_by_email(email)
+    @@record.find { |course| course.email = email && !course.deleted_at }
   end
 end
